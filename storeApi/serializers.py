f0e952f045a608b6200fd4720e073db646824911
Arlefreak <arlefreak@gmail.com>
2015-10-14 22:51:29 -0500

    Added pk to serializer

--- storeApi/serializers.py
+++ storeApi/serializers.py
@@ -19,12 +19,12 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImages
-        fields = ('product', 'name', 'image', 'order', 'date', 'updated',)
+        fields = ('pk','product', 'name', 'image', 'order', 'date', 'updated',)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username','first_name', 'last_name', 'email')
+        fields = ('pk','username','first_name', 'last_name', 'email')
     def create(self, validated_data):
         client_data = validated_data.pop('client', None)
         user = super(ClientSerializer, self).create(validated_data)
@@ -42,20 +42,20 @@ class UserSerializer(serializers.ModelSerializer):
 class ShoppingCartProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingCartProduct
-        fields = ('client', 'product', 'cuantity')
+        fields = ('pk','client', 'product', 'cuantity')
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ('sku','client', 'shippingAdress', 'billingAdress', 'items_subTotal', 'shipping_cost', 'total', 'shipping_carrier', 'shipping_tracking', 'date', 'updated', 'status',)
+        fields = ('pk','sku','client', 'shippingAdress', 'billingAdress', 'items_subTotal', 'shipping_cost', 'total', 'shipping_carrier', 'shipping_tracking', 'date', 'updated', 'status',)
 
 class AdressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Adress
-        fields = ('client', 'name', 'type', 'default', 'firstname', 'lastname', 'adress_line1', 'adress_line2', 'city', 'state_province', 'country', 'zipcode', 'phone_number', 'date',)
+        fields = ('pk','client', 'name', 'type', 'default', 'firstname', 'lastname', 'adress_line1', 'adress_line2', 'city', 'state_province', 'country', 'zipcode', 'phone_number', 'date',)
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('name',)
+        fields = ('pk','name',)
 
